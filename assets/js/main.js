b272d384c0bd5d24b4ca63b1a45bf124392b2b6b
Jeremias <jeremiaspion@gmail.com>
2022-08-26 09:45:11 -0300

    Finalização implementação inicial

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,38 +1,11 @@
-/* let count = 0;
-let iconOpen = document.getElementById("icon-open");
-let iconClose = document.getElementById("icon-close");
-
-iconClose.addEventListener("click", exit);
-
-function exit(){
-    
-}
-
-let menuOpen = document.getElementById("menu-open").addEventListener("click", openAndCloseMenu);
-
-function openAndCloseMenu() {
-    if(count % 2 == 0){
-        iconOpen.classList.add("icon-hidden");
-        iconClose.classList.remove("icon-hidden");
-    }
-    else{
-        iconOpen.classList.remove("icon-hidden");
-        iconClose.classList.add("icon-hidden");
-    }
-    count++;
-} */
-/* 
-let ratos = document.querySelector(".ratos").addEventListener("mouseenter", () => {
-    let hover = document.querySelector(".hover");
-    hover.style.display = "block";
-});
-
-let ratos1 = document.querySelector(".ratos").addEventListener("mouseout", () => {
-    let hover = document.querySelector(".hover");
-    hover.style.display = "none";
-}); */
-
-let ratos = document.getElementById("a-dos-ratos").addEventListener("click", () => {
-    let ratos1 = document.getElementById("barao-do-triunfo  ").classList.add("teste");
-}); 
-
+/* ANIMAÇÃO CONFORME FOR DANDO SCROLL */
+ScrollReveal({
+    origin: 'top',
+    distance: '30px',
+    duration: 700,
+}).reveal(`
+    navbar-nav,
+    #baixo,
+    .image-main,
+    .group-cities
+`);
